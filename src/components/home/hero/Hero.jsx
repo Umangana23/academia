66585035b55a31f8df724cfb74baf366005d3138
Umangana23 <umanganakhuteta04@gmail.com>
2024-03-12 15:08:41 +0530

    first commit

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
+            <p>Embark on a journey of knowledge with our online education hub. Our diverse courses cater to various interests and skill levels, fostering an interactive and enriching learning experience. Join us and redefine your educational path, shaping a brighter future at your own pace.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
